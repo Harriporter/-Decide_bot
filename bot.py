7fdc4e67ea6adb4f379e7005530022987ae3f0be
Harriporter <Harrislin6369@gmail.com>
2021-01-18 17:28:35 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,15 +11,20 @@ async def on_ready():
 @bot.event
 async def  on_member_join(member):
     channel = bot.get_channel(800648407236345866)
-    print(f'{member} joins!')
+    #print(f'{member} joins!')
     await channel.send(f'{member} join!')
 
 @bot.event
 async def  on_member_remove(member):
     channel = bot.get_channel(800648419848617994)
-    print(f'{member} leave!')
+    #print(f'{member} leave!')
     await channel.send(f'{member} leave!')
 
-bot.run('ODAwNTUyNzQyODk1NzQ3MTIz.YATy1w.AdsOkrdmbkRMuqsFP4PF-DTaqV8')
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)}(ms)')
+
+
+bot.run('ODAwNTUyNzQyODk1NzQ3MTIz.YATy1w.NAjfIJPIGD_feZwH2Vkbwm7I70M')
 
 
